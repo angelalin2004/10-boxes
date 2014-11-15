df0c21f4fef8674c1ab5a991f785412d21f7e0b3
angelalin2004 <adl040@ucsd.edu>
2014-11-14 18:42:01 -0800

    Added instructions and Randomize option

--- js/index.js
+++ js/index.js
@@ -32,8 +32,6 @@ function drop(ev) {
       counter++;   
     }
   
- 
-      // document.getElementById("test").innerHTML += "<br>" + shift_indexes[i];
     var one = 0, two = 0;
     var end = shift_indexes.length-1;
     for ( var i = end; i >= 0; i-- ) {
@@ -53,70 +51,40 @@ function drop(ev) {
         document.getElementById("drag"+shift_indexes[i]).src = document.getElementById("drag"+shift_indexes[i-1]).src;
       }
       
-      
-      
-      /*
-      if ( i == 1 ) {
-        document.getElementById("drag"+shift_indexes[i]).src = one;
-      }
-      else if ( i == 0 ) {
-        document.getElementById("drag"+shift_indexes[i]).src = two;
-      }
-      else {
-        var ind = shift_indexes[i-2];
-        document.getElementById("drag"+shift_indexes[i]).src = document.getElementById("drag"+shift_indexes[i-2]).src;
-      }
-      */
     }  
-  
-    /*
-    var one = 0, two = 0;
-    var count = 0;
-    for ( var i = 10; i >= 1; i-- ) {
-      var source = document.getElementById("drag"+i).src;
-      
-      // skip the dragged and dropped-on objects 
-      if ( source == ev.target.src || source == document.getElementById(data).src ) {
-        continue; 
-      }
-      
-      if ( one == 0 ) {
-        one = source;
-        document.getElementById("test").innerHTML += "<br>one: " + one;
-      }
-      else if ( one != 0 && two == 0 ) {
-        two = source;
-      }
-      
-      if ( count == 6 ) {
-        document.getElementById("drag"+i).src = one;
-      }
-      else if ( count == 7 ) {
-        document.getElementById("drag"+i).src = two;
-      }
-      else {
-        minus2 = i-2;
-        // Don't change to dragged/dropped-on objects
-        for ( var j = 0; j <= 2; j++ ) {
-          if ( document.getElementById("drag"+minus2).src == ev.target.src || document.getElementById("drag"+minus2).src == document.getElementById(data).src )
-            minus2--;
-        }
-        
-        document.getElementById("drag"+i).src = document.getElementById("drag"+minus2).src;
-      }
-      count++;
-      
-    }  
-    */
 }
 
-function swap ( a, b ) {
-  
+function swap ( a, b ) { 
   var temp = a;
   document.getElementById("test").innerHTML += "<br" + temp;
   a = b;
   document.getElementById("test").innerHTML += "<br>a: " + a;
   b = temp;
-  document.getElementById("test").innerHTML += "<br>b: " + b;
+  document.getElementById("test").innerHTML += "<br>b: " + b; 
+}
+
+var sources_const = [];
+
+function randomize ( ) {
+
+  for ( var i = 1; i <= 10; i++ ) {
+    /* stores all srcs */
+    sources_const[i-1] = document.getElementById("drag"+i).src;
+  }
   
+  var sources = sources_const;
+  var ind;
+  for ( var i = 1; i <= 10; i++ ) {
+    do {
+      ind = Math.floor(Math.random()*10);
+    } while ( sources[ind] == 0 );
+    document.getElementById("drag"+i).src = sources[ind];
+    sources[ind] = 0;
+  }
+}
+
+function reset ( ) {
+  for ( var i = 1; i <= 10; i++ ) {
+    document.getElementById("drag"+i).src = sources_const[ind-1];
+  }
 }
